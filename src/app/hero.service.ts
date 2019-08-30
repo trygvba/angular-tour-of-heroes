2d9af192d9ba3747c9f8b6931918bf496eb1ec5c
Trygve Bærland <trygve.baerland@gmail.com>
2019-08-30 20:55:14 +0200

    Done with routing part of introduction.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,4 +17,11 @@ export class HeroService {
       private messageService: MessageService
       ) { 
   }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
+
